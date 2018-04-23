c470e7d5c161714c642c459494ae27d4f9ad299a
John <mrfrenik@gmail.com>
2018-04-23 08:41:47 -0500

    stuff

--- Source/Graphics/Skeleton.cpp
+++ Source/Graphics/Skeleton.cpp
@@ -39,3 +39,31 @@ namespace Enjon
 
 	//==================================================================== 
 }
+
+
+
+/*
+	
+	// Used to calculate local space transform for all bones
+	void CalculateTransform( const u32& boneID, const Mat4x4& parentMatrix, const Skeleton* skeleton, Vector<Matrix4x4>& outMatrices )
+	{
+		// Get bone
+		Bone* bone = skeleton->mBones.at( bondID );
+
+		// Calculate bone transform ( bone space )
+		Mat4x4 boneTransform = matTrans * matRotation * matScale;
+
+		// Calculate relative to parent
+		Mat4x4 relativeTransform = parentMatrix * boneTransform;
+
+		// Calculate and set local space matrix
+		outMatrices.at( bone->mID ) = skeleton->mGlobalInverseTransform * relativeTransform * bone->mOffsetTransform;
+
+		// Iterate through children
+		for ( u32 i = 0; i < bone->mChildren; ++i )
+		{
+			CalculateTransform( bone->mChildren.at( i ), relativeTransform, skeleton, outMatrices );				
+		}
+	} 
+
+*/
